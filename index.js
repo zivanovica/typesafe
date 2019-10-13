d347fdbfc2bf7a00209c7bf7ff25918a8debffe8
Aleksandar Zivanovic <coapsyfactor@gmail.com>
2019-10-14 00:53:54 +0200

    Provided better error message

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-const validateType = ({field, value, fieldType, allowNull}) => {
-    if (null === value && false === allowNull) {
-        throw new Error(`Property "${field}" or it's item is not nullable`);
-    }
-
+/**
+ * Perform type validation upon provided value.
+ *
+ * @param {*} value Value that is being asserted.
+ * @param {*} fieldType Expected type.
+ * @param {boolean} allowNull Flag that determines whether or not to allow null.
+ * @param {string} error Error message.
+ *
+ * @throws {TypeError} Validation error.
+ */
+const validateType = ({value, fieldType, allowNull, error}) => {
     if (
-        null !== fieldType && null !== value && undefined !== value && value.constructor !== fieldType
+        (null === value && false === allowNull) ||
+        (null !== fieldType && null !== value && undefined !== value && value.constructor !== fieldType)
     ) {
-        throw new TypeError(
-            `Invalid property or it's item type "${field}" expected "${fieldType.name}" got "${value.constructor.name}"`
-        );
+        throw new TypeError(error);
     }
 };
 
 /**
  * Set type safety to object.
  *
- * @param {*} source Object to which type safety will be applied on.
+ * @param {Object} source Object to which type safety will be applied on.
  * @param {Object} definition Type safety definition.
- * @param {{unknown: boolean}} options Additional options.
+ * @param {Object} options Additional options.
+ * @param {boolean} options.unknown Flag that determines whether or not to allow parameters that are not defined in definition.
  * @returns {*} source object.
+ *
+ * @throws {TypeError} Validation error.
  */
-const makeTypeSafe = function (source, definition, {unknown = true} = {}) {
+const makeTypeSafe = function (source, {...definition} = {}, {...options} = {}) {
+    const {unknown = true} = options;
+
     let sourceObject = source;
 
     Object
@@ -36,17 +46,30 @@ const makeTypeSafe = function (source, definition, {unknown = true} = {}) {
                 defaultValue = undefined
             } = typeof fieldDefinition === 'object' ? fieldDefinition : {type: fieldDefinition};
 
+            const {name: fieldTypeName = 'null'} = null === fieldType ? {} : fieldType;
+            const {name: fieldItemTypeName = 'null'} = null === fieldItemType ? {} : fieldItemType;
+
             Object.defineProperty(sourceObject, field, {
                 set(value) {
-                    validateType({field, value, allowNull, fieldType});
+                    const valueTypeName = value && value.constructor.name || 'null';
+
+                    validateType({
+                        value,
+                        allowNull,
+                        fieldType,
+                        error: `Invalid "${field}" property type, expected "${fieldTypeName}" got "${valueTypeName}"`,
+                    });
 
                     if (value instanceof Array && null !== fieldItemType) {
                         value.forEach((item) => {
+                            const itemTypeName = item && item.constructor.name || 'null';
+
                             validateType({
                                 field,
                                 fieldType: fieldItemType,
                                 allowNull: fieldItemAllowNull,
-                                value: item
+                                value: item,
+                                error: `Invalid "${field}" property item type, expected "${fieldItemTypeName}" got "${itemTypeName}"`
                             });
                         });
                     }
@@ -65,7 +88,7 @@ const makeTypeSafe = function (source, definition, {unknown = true} = {}) {
         sourceObject = new Proxy(sourceObject, {
             set: (target, property, value) => {
                 if (typeof definition[property] === 'undefined') {
-                    throw new Error(`Property "${property}" not allowed.`);
+                    throw new TypeError(`Property "${property}" not allowed.`);
                 }
 
                 target[property] = value;
